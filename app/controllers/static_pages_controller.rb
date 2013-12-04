c3740ed91a3d0fe7300d7969170f57fb506104e6
Juan Sanchez <juansahe@gmail.com>
2013-12-04 00:05:08 -0500

    Add user microposts

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+  	if signed_in?
+      @micropost  = current_user.microposts.build
+      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 6)
+    end
   end
 
   def help
@@ -10,5 +14,4 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
-  	
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
-  before_action :signed_in_user, only: [:index, :edit, :update, :destroy]
-  before_action :correct_user,   only: [:edit, :update]
-  before_action :admin_user,     only: :destroy
+  before_action :signed_in_user,  only: [:index, :edit, :update, :destroy]
+  before_action :correct_user,    only: [:edit, :update]
+  before_action :admin_user,      only: :destroy
+  before_action :already_signed,  only: [:new, :create]
 
   def index
     @users = User.paginate(page: params[:page])
@@ -13,6 +14,7 @@ class UsersController < ApplicationController
 
   def show
   	@user = User.find(params[:id])
+    @microposts = @user.microposts.paginate(page: params[:page], :per_page => 8)
   end
 
   def create
@@ -50,13 +52,6 @@ class UsersController < ApplicationController
     end
 
     # Before filters
-    def signed_in_user
-      unless signed_in?
-        store_location
-        redirect_to signin_url, notice: "Please sign in."
-      end
-    end
-
     def correct_user
       @user = User.find(params[:id])
       redirect_to(root_url) unless current_user?(@user)
@@ -64,6 +59,12 @@ class UsersController < ApplicationController
 
     def admin_user
       redirect_to(root_url) unless current_user.admin?
+    end
+
+    def already_signed
+      if signed_in?
+        redirect_to current_user, notice: "You must signout first."
+      end
     end
 
 end 
